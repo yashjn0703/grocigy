8824f719b3e1e0c69c336b6a818db54cde0ce1e9
yashjn0703 <69392235+yashjn0703@users.noreply.github.com>
2022-09-18 12:27:24 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function doSomething() {
+  alert("Item added to cart");
+}
+
+function signin() {
+  alert("You are now signed in, Welcome to Grocify!");
+}
+
+function sendMessage() {
+  alert("Message Sent, Thank you for contacting us!")
+}
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
